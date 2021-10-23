fe3cc2ade709c018ddbf5fb70cd2b27e712c28b7
Robin <midouesterner@gmail.com>
2021-10-23 13:35:10 -0500

    chore: add django rest framework

--- config/settings.py
+++ config/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
+    # Django
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third-party
+    "rest_framework",
+    # Project
     "accounts",
     "games",
 ]
