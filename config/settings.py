20feb076e49e75f60c0f7652cdda82df50903550
Robin <midouesterner@gmail.com>
2021-10-31 16:41:44 -0500

    fix(filter): search filter appears first in browsable api

--- config/settings.py
+++ config/settings.py
@@ -155,8 +155,8 @@ REST_FRAMEWORK = {
         "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
     ],
     "DEFAULT_FILTER_BACKENDS": [
-        "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.SearchFilter",
+        "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.OrderingFilter",
     ],
 }
