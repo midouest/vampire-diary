161efcd258e6f10aa4212f9192496258b5b20c9e
Robin <midouesterner@gmail.com>
2021-11-07 14:26:00 -0600

    feat(web): diary page loads deep vampire

--- web/src/diary/DiaryPage.tsx
+++ web/src/diary/DiaryPage.tsx
@@ -2,6 +2,7 @@ import { useAppDispatch } from "app/hooks";
 import { useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import { useParams } from "react-router";
+import { retrieveDeepVampire } from "./diary-thunk";
 
 export function DiaryPage() {
   const { id } = useParams();
@@ -9,7 +10,9 @@ export function DiaryPage() {
 
   const vampireId = parseInt(id!);
 
-  useEffect(() => {}, [vampireId, dispatch]);
+  useEffect(() => {
+    dispatch(retrieveDeepVampire(vampireId));
+  }, [vampireId, dispatch]);
 
   return (
     <Container>
