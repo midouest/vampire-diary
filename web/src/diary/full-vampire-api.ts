38890b028ffa6af5798a0b9024a1315f21203d11
Robin <midouesterner@gmail.com>
2021-11-07 10:52:46 -0600

    refactor(web): move models into full vampire model

--- web/src/diary/full-vampire-api.ts
+++ web/src/diary/full-vampire-api.ts
@@ -1,5 +1,26 @@
+import { createCrudApi } from "common/api";
 import { FetchApi } from "common/fetch";
-import { FullVampire } from "./full-vampire-model";
+import {
+  Character,
+  CreateCharacterFormData,
+  CreateExperienceFormData,
+  CreateMarkFormData,
+  CreateMemoryFormData,
+  CreateResourceFormData,
+  CreateSkillFormData,
+  Experience,
+  FullVampire,
+  Mark,
+  Memory,
+  Resource,
+  Skill,
+  UpdateCharacterFormData,
+  UpdateExperienceFormData,
+  UpdateMarkFormData,
+  UpdateMemoryFormData,
+  UpdateResourceFormData,
+  UpdateSkillFormData,
+} from "./full-vampire-model";
 
 export async function retrieveFullVampireApi(
   fetchApi: FetchApi,
@@ -8,3 +29,57 @@ export async function retrieveFullVampireApi(
   const res = await fetchApi(`/api/v1/game/vampires/${id}/full/`);
   return res.json();
 }
+
+function baseUrl(resource: string): string {
+  return `/api/v1/game/${resource}/`;
+}
+
+export const {
+  createApi: createMemoryApi,
+  updateApi: updateMemoryApi,
+  removeApi: removeMemoryApi,
+} = createCrudApi<Memory, CreateMemoryFormData, UpdateMemoryFormData>(
+  baseUrl("memories")
+);
+
+export const {
+  createApi: createExperienceApi,
+  updateApi: updateExperienceApi,
+  removeApi: removeExperienceApi,
+} = createCrudApi<
+  Experience,
+  CreateExperienceFormData,
+  UpdateExperienceFormData
+>(baseUrl("experiences"));
+
+export const {
+  createApi: createSkillApi,
+  updateApi: updateSkillApi,
+  removeApi: removeSkillApi,
+} = createCrudApi<Skill, CreateSkillFormData, UpdateSkillFormData>(
+  baseUrl("skills")
+);
+
+export const {
+  createApi: createResourceApi,
+  updateApi: updateResourceApi,
+  removeApi: removeResourceApi,
+} = createCrudApi<Resource, CreateResourceFormData, UpdateResourceFormData>(
+  baseUrl("resources")
+);
+
+export const {
+  createApi: createCharacterApi,
+  updateApi: updateCharacterApi,
+  removeApi: removeCharacterApi,
+} = createCrudApi<Character, CreateCharacterFormData, UpdateCharacterFormData>(
+  baseUrl("characters")
+);
+
+export const {
+  createApi: createMarkApi,
+  updateApi: updateMarkApi,
+  removeApi: removeMarkApi,
+} = createCrudApi<Mark, CreateMarkFormData, UpdateMarkFormData>(
+  baseUrl("marks")
+);
--- web/src/diary/full-vampire-model.ts
+++ web/src/diary/full-vampire-model.ts
@@ -2,7 +2,122 @@ export interface FullVampire {
   id: number;
   name: string;
   description: string;
-  //   memories: Memory[];
-  //   skills: Skill[];
-  //   resources: Resource[];
+  memories: Memory[];
+  skills: Skill[];
+  resources: Resource[];
+  characters: Character[];
+  marks: Mark[];
+}
+
+export interface Memory {
+  id: number;
+  vampire: number;
+  experiences: Experience[];
+  diary: number | null;
+  isForgotten: boolean;
+}
+
+export interface CreateMemoryFormData {
+  vampire: number;
+}
+
+export interface UpdateMemoryFormData {
+  id: number;
+  diary: number | null;
+  isForgotten: boolean;
+}
+
+export interface Experience {
+  id: number;
+  memory: number;
+  description: string;
+}
+
+export interface CreateExperienceFormData {
+  memory: number;
+  description: string;
+}
+
+export interface UpdateExperienceFormData {
+  id: number;
+  description: string;
+}
+
+export interface Skill {
+  id: number;
+  vampire: number;
+  description: string;
+  isChecked: boolean;
+}
+
+export interface CreateSkillFormData {
+  vampire: number;
+  description: string;
+}
+
+export interface UpdateSkillFormData {
+  id: number;
+  description: string;
+  isChecked: boolean;
+}
+
+export interface Resource {
+  id: number;
+  vampire: number;
+  description: string;
+  isDiary: boolean;
+  isLost: boolean;
+}
+
+export interface CreateResourceFormData {
+  vampire: number;
+  description: string;
+  isDiary: boolean;
+}
+
+export interface UpdateResourceFormData {
+  id: number;
+  description: string;
+  isDiary: boolean;
+  isLost: boolean;
+}
+
+export interface Character {
+  id: number;
+  vampire: number;
+  name: string;
+  description: string;
+  isImmortal: boolean;
+  isDead: boolean;
+}
+
+export interface CreateCharacterFormData {
+  vampire: number;
+  name: string;
+  description: string;
+  isImmortal: boolean;
+}
+
+export interface UpdateCharacterFormData {
+  id: number;
+  name: string;
+  description: string;
+  isImmortal: boolean;
+  isDead: boolean;
+}
+
+export interface Mark {
+  id: number;
+  vampire: number;
+  description: string;
+}
+
+export interface CreateMarkFormData {
+  vampire: number;
+  description: string;
+}
+
+export interface UpdateMarkFormData {
+  id: number;
+  description: string;
 }
