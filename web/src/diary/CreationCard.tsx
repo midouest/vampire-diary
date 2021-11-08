c7bf29858eef58cd0cf461de568bee8c1a92c23f
Robin <midouesterner@gmail.com>
2021-11-08 01:36:02 -0600

    fix(web): move ol outside of p

--- web/src/diary/CreationCard.tsx
+++ web/src/diary/CreationCard.tsx
@@ -17,34 +17,32 @@ export function CreationCard({ vampireId }: CreationCardProps) {
       <Card.Body>
         <Card.Text>To create your vampire:</Card.Text>
 
-        <Card.Text>
-          <ol>
-            <li>Give your vampire a name and description</li>
-            <li>
-              Create a single memory and experience that describes your
-              vampire's history as a mortal
-            </li>
-            <li>Create at least three mortal characters</li>
-            <li>
-              Give your vampire three skills that they practiced as a mortal
-            </li>
-            <li>
-              Give your vampire three resources that they acquired as a mortal
-            </li>
-            <li>
-              Create three memories, each with one experience relating to any
-              two characters, skills and/or resources that you have created
-            </li>
-            <li>
-              Create an immortal character who gifted or cursed your vampire
-              with undeath
-            </li>
-            <li>
-              Create a mark, memory and experience relating to how your vampire
-              was gifted or cursed with undeath
-            </li>
-          </ol>
-        </Card.Text>
+        <ol>
+          <li>Give your vampire a name and description</li>
+          <li>
+            Create a single memory and experience that describes your vampire's
+            history as a mortal
+          </li>
+          <li>Create at least three mortal characters</li>
+          <li>
+            Give your vampire three skills that they practiced as a mortal
+          </li>
+          <li>
+            Give your vampire three resources that they acquired as a mortal
+          </li>
+          <li>
+            Create three memories, each with one experience relating to any two
+            characters, skills and/or resources that you have created
+          </li>
+          <li>
+            Create an immortal character who gifted or cursed your vampire with
+            undeath
+          </li>
+          <li>
+            Create a mark, memory and experience relating to how your vampire
+            was gifted or cursed with undeath
+          </li>
+        </ol>
 
         <Card.Text>
           Your vampire should now have three skills, three resources, one mark,
