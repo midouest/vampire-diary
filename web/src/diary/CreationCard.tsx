28955205fb8f56a7813e4b6584b73c41fd03c0cd
Robin <midouesterner@gmail.com>
2021-11-08 08:51:36 -0600

    feat(web): improve creation card wording

--- web/src/diary/CreationCard.tsx
+++ web/src/diary/CreationCard.tsx
@@ -15,39 +15,39 @@ export function CreationCard({ vampireId }: CreationCardProps) {
     <Card>
       <Card.Header>Creation</Card.Header>
       <Card.Body>
-        <Card.Text>To create your vampire:</Card.Text>
+        <Card.Text>To create your Vampire:</Card.Text>
 
         <ol>
-          <li>Give your vampire a name and description</li>
+          <li>Give your Vampire a name and description</li>
           <li>
-            Create a single memory and experience that describes your vampire's
+            Create a single Memory and Experience that describes your Vampire's
             history as a mortal
           </li>
-          <li>Create at least three mortal characters</li>
+          <li>Create at least three mortal Characters</li>
           <li>
-            Give your vampire three skills that they practiced as a mortal
+            Give your Vampire three Skills that they practiced as a mortal
           </li>
           <li>
-            Give your vampire three resources that they acquired as a mortal
+            Give your Vampire three Resources that they acquired as a mortal
           </li>
           <li>
-            Create three memories, each with one experience relating to any two
-            characters, skills and/or resources that you have created
+            Create three Memories, each with one Experience relating to any two
+            Characters, Skills and/or Resources that you have created
           </li>
           <li>
-            Create an immortal character who gifted or cursed your vampire with
+            Create an immortal Character who gifted or cursed your Vampire with
             undeath
           </li>
           <li>
-            Create a mark, memory and experience relating to how your vampire
-            was gifted or cursed with undeath
+            Create a Mark, a Memory and an Experience relating to how your
+            Vampire was gifted or cursed with undeath
           </li>
         </ol>
 
         <Card.Text>
-          Your vampire should now have three skills, three resources, one mark,
-          at least three mortals, one immortal, and five memories each with one
-          experience.
+          Your Vampire should now have three Skills, three Resources, one Mark,
+          at least three mortal Characters, one immortal Character, and five
+          Memories each with one Experience.
         </Card.Text>
 
         <Button variant="outline-success" size="sm" onClick={handleStart}>
